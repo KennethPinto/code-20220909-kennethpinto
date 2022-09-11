f6a25fd233563bf8c70b97d358cbf0a11929a7f4
Kenneth Antony Pinto <KennethAntonyPinto@MacBook-Pro.local>
2022-09-11 12:14:14 +0530

    Updated the code to get data with promises

--- src/routes/bmiroutes.ts
+++ src/routes/bmiroutes.ts
@@ -23,46 +23,50 @@ class BMIRoute {
     private routes(): void {
         this.express.post("/bmi-calculator", (req, res, next) => {
             let users = this.requestTransformer(req.body);
-            let responseBody = users.map(user => this.calculateBMIAndGetBMICategoryAndHealthRisk(user));
-            res.json(this.responseTransformer(responseBody));
+            let promises = [];
+            promises.push(users.map(user => this.calculateBMI(user)));
+            Promise.all(promises).then((user) => {
+                users.push(user);
+            });
+            promises.push(users.map(user => this.getBMICategoryAndHealthRisk(user)));
+            Promise.all(promises).then((user) => {
+                users.push(user);
+            });
+            res.json(this.responseTransformer(users));
         });
     }
 
-    private calculateBMIAndGetBMICategoryAndHealthRisk(user) {
-        let mass = user.mass;
-        let height = user.height;
-        let bmi = mass/Math.pow(height, 2);
-        user = {
-            "gender": user.gender,
-            "mass": mass,
-            "height": height,
-            "bmi": bmi
-        };
-        return this.getBMICategoryAndHealthRisk(user);
+    private calculateBMI(user) {
+        return new Promise((resolve, reject) => {
+            user['bmi'] = user.mass/Math.pow(user.height, 2);
+            resolve(user);
+          });
     }
     
     private getBMICategoryAndHealthRisk(user) {
-        let bmi = user.bmi;
-        if(bmi<18.5) {
-            user['bmiCategory'] = "Underweight";
-            user['healthRisk'] = "Malnutrition risk";
-        } else if(bmi>=18.5 && bmi<=24.9) {
-            user['bmiCategory'] = "Normal weight";
-            user['healthRisk'] = "Low risk";
-        } else if(bmi>=25 && bmi<=29.9) {
-            user['bmiCategory'] = "Overweight";
-            user['healthRisk'] = "Enhanced risk";
-        } else if(bmi>=30 && bmi<=34.9) {
-            user['bmiCategory'] = "Moderately obese";
-            user['healthRisk'] = "Medium risk";
-        } else if(bmi>=35 && bmi<=39.9) {
-            user['bmiCategory'] = "Severely obese";
-            user['healthRisk'] = "High risk";
-        } else {
-            user['bmiCategory'] = "Very severely obese";
-            user['healthRisk'] = "Very high risk";
-        }
-        return user;
+        return new Promise((resolve, reject) => {
+            let bmi = user.bmi;
+            if(bmi<18.5) {
+                user['bmiCategory'] = "Underweight";
+                user['healthRisk'] = "Malnutrition risk";
+            } else if(bmi>=18.5 && bmi<=24.9) {
+                user['bmiCategory'] = "Normal weight";
+                user['healthRisk'] = "Low risk";
+            } else if(bmi>=25 && bmi<=29.9) {
+                user['bmiCategory'] = "Overweight";
+                user['healthRisk'] = "Enhanced risk";
+            } else if(bmi>=30 && bmi<=34.9) {
+                user['bmiCategory'] = "Moderately obese";
+                user['healthRisk'] = "Medium risk";
+            } else if(bmi>=35 && bmi<=39.9) {
+                user['bmiCategory'] = "Severely obese";
+                user['healthRisk'] = "High risk";
+            } else {
+                user['bmiCategory'] = "Very severely obese";
+                user['healthRisk'] = "Very high risk";
+            }
+            resolve(user);
+        });
     }
     
     private requestTransformer(users) {
